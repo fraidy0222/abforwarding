c5be2ed26f6b5b475985c261098b0cce72a46cde
Fraidy <fraidygonzalezreinaldo@gmail.com>
2025-05-30 10:42:25 -0400

    add components header and movil header

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <HeaderAppHeader class="hidden md:block" />
+    <HeaderAppHeaderMovil class="md:hidden block" />
+    <slot />
+    <!-- <AppFooter /> -->
+  </div>
+</template>
+
+<script setup></script>
