58ac573aed2da0de9dc286470adac6bc1d956c13
Fraidy <fraidygonzalezreinaldo@gmail.com>
2025-05-30 10:37:49 -0400

    add i18n

---
+++ components/Header/AppHeader.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { computed } from "vue";
+const { t } = useI18n();
+const localePath = useLocalePath();
+
+const navLinks = computed(() => [
+  { name: t("header.inicio"), path: "/" },
+  // { name: t("header.inicio"), path: "/" },
+  { name: t("header.servicios"), path: "/" },
+  { name: t("header.sobre-nosotros"), path: "/" },
+  { name: t("header.sucursales"), path: "/" },
+  { name: t("header.contacto"), path: "/" },
+]);
+</script>
+
+<template>
+  <header class="bg-white shadow-sm sticky top-0 z-50">
+    <div class="container mx-auto px-4 py-3 flex justify-between items-center">
+      <!-- Logo -->
+      <!-- <NuxtLink to="/" class="flex items-center">
+        <img src="/images/logo.svg" alt="AB Forwarding" class="h-14" />
+      </NuxtLink> -->
+
+      <div>Logo</div>
+      <!-- Navegación -->
+      <nav class="hidden md:flex items-center gap-8">
+        <NuxtLink
+          v-for="link in navLinks"
+          :key="link.name"
+          :to="localePath(link.path)"
+          class="font-montserrat font-medium text-dark hover:text-accent transition-colors duration-300"
+          :class="{ 'text-primary': $route.path === link.path }"
+        >
+          {{ link.name }}
+        </NuxtLink>
+      </nav>
+
+      <!-- Acciones -->
+      <div class="flex items-center gap-6">
+        <HeaderLanguageSwitcher />
+        <!-- <LanguageSwitcher /> -->
+      </div>
+    </div>
+  </header>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,30 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
+
+  ui: {
+    colorMode: false,
+  },
   modules: [
     "@nuxt/eslint",
     "@nuxt/fonts",
     "@nuxt/icon",
     "@nuxt/image",
     "@nuxt/ui",
+    "@nuxtjs/i18n",
   ],
+
+  i18n: {
+    defaultLocale: "es",
+    strategy: "prefix_except_default",
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: "i18n_redirected",
+    },
+    lazy: true,
+    locales: [
+      { code: "es", name: "Español", file: "es.json" },
+      { code: "ru", name: "Русский", file: "ru.json" },
+    ],
+  },
 });
