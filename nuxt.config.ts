c86ea83971f3489045a5bdfd6d8b2cf29867bd6d
Fraidy <fraidygonzalezreinaldo@gmail.com>
2025-06-04 14:47:14 -0400

    other things of worflows

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/abforwarding/",
+    buildAssetsDir: "_nuxt/",
+  },
   ssr: false,
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
