f5a3c22623f9508e4542ec2fdaa5dc4fdcf7c406
Fraidy <fraidygonzalezreinaldo@gmail.com>
2025-06-14 20:45:46 -0400

    add title page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
+    head: {
+      title: "ABForwarding",
+      charset: "utf-16",
+      viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+      htmlAttrs: {
+        lang: "es",
+      },
+    },
     baseURL: "/abforwarding/",
     buildAssetsDir: "_nuxt/",
   },
